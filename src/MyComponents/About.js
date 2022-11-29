459668ae8376dde0217f8767356d260847caad76
Ashif Alam <85867457+Ashifalam@users.noreply.github.com>
2022-11-30 01:10:58 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      This is an about components
+      <p>This group can be advertisers, content contributors,
+         site owners in your niche, job seekers, and researchers putting
+          together a feature about you in their blog post, interview, 
+          or school paper. This group is interested in two things: facts 
+          and your history.For example,
+          advertisers might want to know if your
+         site covers the demographic they are targeting.
+      </p>
+    </div>
+  )
+}
+  
